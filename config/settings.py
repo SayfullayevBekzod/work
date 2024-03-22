4f45e161b3a91f7262edf0c9bce62156b791735d
Bekzod <sayfulleyvbekzod465@gmail.com>
2024-03-23 01:12:15 +0500

    work

--- config/settings.py
+++ config/settings.py
@@ -66,13 +66,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'work',
-        'USER': 'postgres',
-        'PASSWORD': '0000',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
